4a9c914b3bac8af49940f36caa2c4fe67909c3c3
jabiro <jabichris@gmail.com>
2019-08-06 20:35:43 +0200

    heroku setup

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import path from 'path';
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static(__dirname + '/build'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname + '/build', 'index.html'));
+});
+
+app.listen(port);
